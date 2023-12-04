5b9cf5688b2e6c82ba34bb1f38b5041dae96b4d7
Cyril Vallez <cyril.vallez@gmail.com>
2023-12-04 18:43:47 +0100

    Correctly add data files to package

--- textwiz/__init__.py
+++ textwiz/__init__.py
@@ -10,7 +10,7 @@ from textwiz.streamer import TextContinuationStreamer
 from textwiz import warnings_suppressor
 
 
-__version__ = '0.0.1'
+__version__ = '0.0.4'
 
 
 def is_chat_model(model_name: str) -> bool:
