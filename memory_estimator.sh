ddf7c0dd8ace2b6c6a6468438289d7deac0221f1
Cyril Vallez <cyril.vallez@gmail.com>
2024-04-19 12:24:28 +0200

    Update memory_estimator.sh

--- memory_estimator.sh
+++ memory_estimator.sh
@@ -4,10 +4,10 @@
 #SBATCH --output=%x-%j.out
 #SBATCH --error=%x-%j.err
 #SBATCH --time=10-00:00:00
-#SBATCH --cpus-per-task=13
-#SBATCH --mem=250G
+#SBATCH --cpus-per-task=15
+#SBATCH --mem=280G
 #SBATCH --partition=nodes
-#SBATCH --gres=gpu:a100:6
+#SBATCH --gres=gpu:a100:7
 #SBATCH --chdir=/cluster/raid/home/vacy/TextWiz
 
 # Initialize the shell to use local conda
