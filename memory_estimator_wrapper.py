c761957d8f629719a75597e433e391a8fc6ae3ec
Cyril Vallez <cyril.vallez@gmail.com>
2024-06-26 15:01:01 +0200

    update multiprocessing

--- memory_estimator_wrapper.py
+++ memory_estimator_wrapper.py
@@ -149,7 +149,7 @@ def dispatch_jobs_srun(gpu_footprints: list[int], num_gpus: int, commands: list[
             full_command = (f'srun --exclusive --exact --ntasks=1 --gpus-per-task={footprint} --cpus-per-task={cpus} '
                             f'--mem={mem}G {executable}')
             # p = subprocess.Popen(shlex.split(full_command), stdout=output_file, stderr=error_file, bufsize=0)
-            p = subprocess.Popen(shlex.split(full_command), stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=0)
+            p = subprocess.Popen(shlex.split(full_command), stdout=sys.stdout, stderr=sys.stderr, bufsize=0)
 
             # Add them to the list of running processes
             processes.append(p)
